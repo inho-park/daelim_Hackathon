c14948e42b383ddb51967597feac529eee5dfead
JunSeong_Park <ttotto0608@naver.com>
2023-05-26 18:00:40 +0900

    first commit

---
+++ src/main/java/com/daelim/daelim_hackathon/DaelimHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.daelim.daelim_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaelimHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaelimHackathonApplication.class, args);
+    }
+
+}
