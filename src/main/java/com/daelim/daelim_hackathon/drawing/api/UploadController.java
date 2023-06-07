b1ead35405a34ddcb00122bf9c3c997a7cb32f63
JunSeong_Park <ttotto0608@naver.com>
2023-06-07 18:23:46 +0900

    feat : uploading file to S3

--- src/main/java/com/daelim/daelim_hackathon/drawing/api/UploadController.java
+++ src/main/java/com/daelim/daelim_hackathon/drawing/api/UploadController.java
@@ -1,25 +1,38 @@
 package com.daelim.daelim_hackathon.drawing.api;
 
-import com.daelim.daelim_hackathon.drawing.dto.FileDetail;
-import com.daelim.daelim_hackathon.drawing.service.FileUploadService;
+import com.daelim.daelim_hackathon.drawing.service.AwsS3Service;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
-@RequestMapping(value = "/upload", produces = MediaType.APPLICATION_JSON_VALUE)
-@RequiredArgsConstructor
-public class UploadController {
-    private final FileUploadService fileUploadService;
+import java.util.List;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/upload")
+public class UploadController {
+    private final AwsS3Service awsS3Service;
+
+    /**
+     * Amazon S3에 파일 업로드
+     * @return 성공 시 200 Success와 함께 업로드 된 파일의 파일명 리스트 반환
+     */
     @PostMapping
-    public ResponseEntity<FileDetail> post(
-            @RequestPart("file") MultipartFile multipartFile) {
-        return ResponseEntity.ok(fileUploadService.save(multipartFile));
+    public ResponseEntity<List<String>> uploadFile(@RequestPart(value = "file", required = false) List<MultipartFile> multipartFile) {
+//        return ApiResponse.success(awsS3Service.uploadFile(multipartFile));
+        awsS3Service.uploadFile(multipartFile);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * Amazon S3에 업로드 된 파일을 삭제
+     * @return 성공 시 200 Success
+     */
+    @DeleteMapping
+    public ResponseEntity<Void> deleteFile(@RequestParam String fileName) {
+        awsS3Service.deleteFile(fileName);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
