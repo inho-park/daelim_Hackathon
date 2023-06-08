751e6e00c7adf75ff3726c03f448123d191d18d0
JunSeong_Park <ttotto0608@naver.com>
2023-06-09 01:02:03 +0900

    feat : adding dto in file

--- src/main/java/com/daelim/daelim_hackathon/drawing/api/UploadController.java
+++ src/main/java/com/daelim/daelim_hackathon/drawing/api/UploadController.java
@@ -1,9 +1,11 @@
 package com.daelim.daelim_hackathon.drawing.api;
 
 import com.daelim.daelim_hackathon.drawing.dto.FileNameDTO;
+import com.daelim.daelim_hackathon.drawing.dto.UploadFileDTO;
 import com.daelim.daelim_hackathon.drawing.service.AwsS3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,11 +17,32 @@ public class UploadController {
     private final AwsS3Service awsS3Service;
 
     /**
-     * Amazon S3에 파일 업로드
-     * @return 성공 시 200 Success와 함께 업로드 된 파일의 파일명 반환
+     * Amazon S3에서 파일 가져오기
+     * @return 성공 시 200 OK와 함께 업로드 된 파일의 파일명 반환
      */
-    @PostMapping
-    public ResponseEntity uploadFile(@RequestPart(value = "file", required = false) MultipartFile multipartFile) {
+    @GetMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getFile(@PathVariable("id") String id) {
+        return null;
+    }
+
+    /**
+     * Amazon S3에서 파일 가져오기
+     * @return 성공 시 200 OK와 함께 업로드 된 파일의 파일명 반환
+     */
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getFile() {
+        return null;
+    }
+
+    /**
+     * Amazon S3에 파일 업로드
+     * @return 성공 시 200 OK와 함께 업로드 된 파일의 파일명 반환
+     */
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity uploadFile(
+            @RequestPart(value = "file", required = false) MultipartFile multipartFile,
+            @RequestBody UploadFileDTO uploadFileDTO
+    ) {
         awsS3Service.uploadFile(multipartFile);
         return new ResponseEntity<>(
                 FileNameDTO.builder().fileName(awsS3Service.uploadFile(multipartFile)).build(),
@@ -31,9 +54,9 @@ public class UploadController {
      * Amazon S3에 업로드 된 파일을 삭제
      * @return 성공 시 200 Success
      */
-    @DeleteMapping
-    public ResponseEntity deleteFile(@RequestParam String fileName) {
-        awsS3Service.deleteFile(fileName);
+    @DeleteMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity deleteFile(@RequestBody FileNameDTO fileNameDTO) {
+        awsS3Service.deleteFile(fileNameDTO.getFileName());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/daelim/daelim_hackathon/novel/NovelController.java
+++
@@ -1,4 +0,0 @@
-package com.daelim.daelim_hackathon.novel;
-
-public class NovelController {
-}
