0f407fe7842bbc167604e5ab214ea70860918336
JunSeong_Park <ttotto0608@naver.com>
2023-06-09 23:30:52 +0900

    db 와 upload file uuid 들을 쿼리로 연동을 지어야함

--- src/main/java/com/daelim/daelim_hackathon/drawing/service/AwsS3Service.java
+++ src/main/java/com/daelim/daelim_hackathon/drawing/service/AwsS3Service.java
@@ -8,6 +8,7 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.daelim.daelim_hackathon.drawing.repo.NovelDrawingRepository;
 import com.daelim.daelim_hackathon.drawing.repo.PageDrawingRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class AwsS3Service {
@@ -31,28 +33,6 @@ public class AwsS3Service {
     private final PageDrawingRepository pageDrawingRepository;
 
 
-//    public List<String> uploadFiles(List<MultipartFile> multipartFile) {
-//        List<String> fileNameList = new ArrayList<>();
-//
-//        // forEach 구문을 통해 multipartFile로 넘어온 파일들 하나씩 fileNameList에 추가
-//        multipartFile.forEach(file -> {
-//            String fileName = createFileName(file.getOriginalFilename());
-//            ObjectMetadata objectMetadata = new ObjectMetadata();
-//            objectMetadata.setContentLength(file.getSize());
-//            objectMetadata.setContentType(file.getContentType());
-//
-//            try(InputStream inputStream = file.getInputStream()) {
-//                amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
-//                        .withCannedAcl(CannedAccessControlList.PublicRead));
-//            } catch(IOException e) {
-//                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
-//            }
-//
-//            fileNameList.add(fileName);
-//        });
-//
-//        return fileNameList;
-//    }
 
     public String uploadFile(MultipartFile multipartFile) {
 
